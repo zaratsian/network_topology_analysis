31d00cee86814da4c08cc46a261f2501512cb903
Dan Zaratsian <dtz001@gmail.com>
2016-12-14 23:31:57 -0500

    Update stream_kafka.py

--- scripts/stream_kafka.py
+++ scripts/stream_kafka.py
@@ -1,5 +1,3 @@
-
-
 import os,sys,re,csv
 import datetime,time
 import random
@@ -8,7 +6,7 @@ from kafka import KafkaProducer
 try:
    data_file = sys.argv[1]
 except:
-   data_file   = '/traceroute_testdata.txt'
+   data_file   = '/traceroute_google_node_detail.txt'
 
 #brokers     = ['seregion03.cloud.hortonworks.com:6667','seregion04.cloud.hortonworks.com:6667']
 #brokers     = ['sandbox.hortonworks.com:6667']
@@ -17,15 +15,16 @@ topic       = 'dztopic1'
 
 #producer = KafkaProducer(bootstrap_servers=brokers, value_serializer=lambda m: json.dumps(m).encode('ascii'))
 producer = KafkaProducer(bootstrap_servers=brokers)
-file     = csv.reader(open(data_file, 'rb'))
-header   = file.next()
 
-for i,row in enumerate(file):
+data   = open(data_file,'rb').read()
+events = data.split('\n') 
+
+for i,event in enumerate(events):
     time.sleep(0.5)
-    record = '|'.join(row)
+    signal_strength = 80.1
+    signal_noise    = 10.1
+    record = event + '|' + str(signal_strength) + '|' + str(signal_noise)    
     print str(record)
     producer.send(topic, record)
-    if i==8:
-        time.sleep(60)
 
 #ZEND
