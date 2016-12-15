639d5d9bc84832a382ea8ef01743abe0ceb68ec4
Dan Zaratsian <dtz001@gmail.com>
2016-12-15 09:02:27 -0500

    Update traceroute_mapper.py

--- scripts/traceroute_mapper.py
+++ scripts/traceroute_mapper.py
@@ -1,28 +1,49 @@
 
 import os,sys,re
 
-infile  = '/tmp/traceroute_google.txt'
-outfile = '/tmp/traceroute_google_mapped.txt'
+infile  = 'traceroute_google.txt'
+
+outfile_topology_map = 'traceroute_google_mapped.txt'
+output_topology_map  = open(outfile_topology_map,'wb')
+
+outfile_node_info    = 'traceroute_google_node_detail.txt'
+output_node_info     = open(outfile_node_info,'wb')
 
 data = open(infile,'rb').read()
 
 events = data.split('\n')
 
-record = '' 
+topology_map = '' 
+node_details = []
 
 for i, event in enumerate(events):
     try:
-        level   = event.split('|')[1].strip()
-        ip      = event.split('|')[2].strip()
+        level     = str(event.split('|')[1].strip())
+        ip        = event.split('|')[2].strip().replace('192.168.0.1||||||||1','localhost|United States|NC|Raleigh|-78.6382|35.7796|Time Warner Cable|Time Warner Cable|1')
+        country   = event.split('|')[4].strip()
+        region    = event.split('|')[5].strip()
+        city      = event.split('|')[6].strip()
+        longitude = event.split('|')[7].strip()
+        latitude  = event.split('|')[8].strip()
+        isp       = event.split('|')[9].strip()
+        org       = event.split('|')[10].strip()
+        
         if str(level) == '1':
-            record = record + '\n' + ip
+            topology_map = topology_map + '\n' + ip
         else:
-            record = record + '|' + ip
+            topology_map = topology_map + '|' + ip
+        
+        node_detail = ip + '|' + country + '|' + region + '|' + city + '|' + longitude + '|' + latitude + '|' + isp + '|' + org + '|' + level + '\n'
+        
+        if node_detail not in node_details:
+            node_details.append(node_detail)
+            output_node_info.write(node_detail)
     except:
         pass
 
-output = open(outfile,'wb')
-output.write(record)
-output.close()
+topology_map = topology_map.replace('|ip_address\n','').replace('192.168.0.1','localhost')
+
+output_topology_map.write(topology_map)
+output_topology_map.close()
 
 #ZEND
