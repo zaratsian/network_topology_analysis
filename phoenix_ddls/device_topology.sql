daa6cef46d4036267fa6577b2b3af8b97e40f999
Dan Zaratsian <dtz001@gmail.com>
2016-12-15 23:07:43 -0500

    Update device_topology.sql

--- phoenix_ddls/device_topology.sql
+++ phoenix_ddls/device_topology.sql
@@ -1,3 +1,3 @@
---/phoenix/bin/psql.py -t device_topology localhost /traceroute_google_mapped.txt
+--/phoenix/bin/psql.py -t DEVICE_TOPOLOGY localhost /traceroute_google_mapped.csv
 CREATE TABLE IF NOT EXISTS DEVICE_TOPOLOGY (
     topology VARCHAR NOT NULL PRIMARY KEY);
